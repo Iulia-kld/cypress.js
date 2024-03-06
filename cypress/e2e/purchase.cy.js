9dbfc9c8d40ce65b9e393ba4f48506d7c6253cee
Julia <118815057+Iulia-kld@users.noreply.github.com>
2024-03-07 00:45:00 +0200

    Update purchase.cy.js

--- cypress/e2e/purchase.cy.js
+++ cypress/e2e/purchase.cy.js
@@ -4,7 +4,7 @@ describe('Покупка аватара', function () {
     it('Покупка аватара', function () {
         cy.visit('https://pokemonbattle.me/'); 
         cy.get(':nth-child(1) > .auth__input').type('osen-125@yandex.ru');
-        cy.get('#password').type('Nikolawa15031988');
+        cy.get('#password').type('ваш пароль');
         cy.get('.auth__button').click();
         cy.get('.header__btns > [href="/shop"]').click();
         cy.get(':nth-child(12) > .shop__button').click();
